16793ef10ee8570b321d8da7c9dba287d2020689
DannyBoyNYC <daniel.deverell@gmail.com>
2018-06-26 21:22:58 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
 import angular from 'angular';
 import ngRoute from 'angular-route';
+import ngAnimate from 'angular-animate';
 
-const app = angular.module('foodApp', ['ngRoute']);
+const app = angular.module('foodApp', ['ngRoute', 'ngAnimate']);
+
 
 app.config(function config($locationProvider, $routeProvider) {
   $routeProvider
-    .when('/', {
-      template: `
-      <div class="wrap">
-        <h1>Home</h1>
-      </div>
-      `
-    })
-    .when('/recipes', {
-      template: '<recipe-list></recipe-list>'
-    });
+  .when('/', {
+    template: '<h1>Home</h1>'
+  })
+  .when('/recipes', {
+    template: '<recipe-list></recipe-list>'
+  })
+  .when('/recipes/:recipeId', {
+    template: '<recipe-detail></recipe-detail>'
+  });
   $locationProvider.html5Mode(true);
 });
+
 
 app.component('recipeList', {
   templateUrl: '/templates/recipes.html',
   controller: function RecipeListController($scope, $http) {
     $http.get('api/recipes')
-      .then(res => {
-        $scope.recipes = res.data;
-        console.log($scope.recipes);
-    })
+    .then(res => {
+      $scope.recipes = res.data;
+    });
+    
+    $scope.deleteRecipe = (index, recipeid) =>
+    $http.delete(`/api/recipes/${recipeid}`)
+    .then(() => $scope.recipes.splice(index, 1));
   }
+});
+
+app.component('recipeDetail', {
+  templateUrl: '/templates/recipe.html',
+  
+  controller: function RecipeDetailController($http, $routeParams, $scope) {
+    $http.get('api/recipes/' + $routeParams.recipeId).then(res => {
+      ($scope.recipe = res.data);
+      console.log($scope.recipe);
+    });
+  }
+  
 });
